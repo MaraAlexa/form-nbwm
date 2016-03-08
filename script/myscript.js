d8f6e5cdda45a214777f7e7a563ce11d11c7e30a
MaraAlexa <maraalexa57@gmail.com>
2016-03-08 21:10:01 +0100

    improved css

--- script/myscript.js
+++ script/myscript.js
@@ -1,13 +1,14 @@
 $(function(){
+	//highlight the link clicked
 		$("ul li a").click(function(){
 			$(this).siblings("li").removeClass('active');
 			$(this).addClass('active');
 		});
 // show the small nav bar
-		$('.menu').click(function(e){
-			e.stopPropagation();
+		$('.menu').click(function(){
+			//e.stopPropagation();
 			$('#main-nav').toggleClass('mobile-active');
 		});
 
 
-	});
+	}); // End wrapping function
