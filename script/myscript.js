4670929091074674171a0833cc7ddfd2cf759b8e
MaraAlexa <maraalexa57@gmail.com>
2016-03-08 11:45:50 +0100

    toggle menu when smaller page

--- script/myscript.js
+++ script/myscript.js
@@ -2,6 +2,12 @@ $(function(){
 		$("ul li a").click(function(){
 			$(this).siblings("li").removeClass('active');
 			$(this).addClass('active');
-			
 		});
+// show the small nav bar
+		$('.menu').click(function(e){
+			e.stopPropagation();
+			$('#main-nav').toggleClass('mobile-active');
+		});
+
+
 	});
